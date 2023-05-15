67a3e2aee0dfb1ac4eae50d76b67f75882bf1f9b
MaryRabinovich <mary.rabinovich@mail.ru>
2023-05-15 14:11:28 +0300

    basic structure for multiregional module

---
+++ modules/multiregional/src/MultiregionalServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Multiregional;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiregionalServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        dd('From multiregional provider');
+    }
+}
